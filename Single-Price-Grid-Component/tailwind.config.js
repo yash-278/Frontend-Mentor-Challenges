43863390ab83e0454579bd462fcb5b27c5baa007
Yash Kadam <yashkadam278@gmail.com>
2022-03-29 13:19:55 +0530

    Single Price Grid Component Complete

---
+++ Single-Price-Grid-Component/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        cyan: "hsl(179, 62%, 43%)",
+        brightYellow: "hsl(71, 73%, 54%)",
+        lightGray: "hsl(204, 43%, 93%)",
+        grayishBlue: "hsl(218, 22%, 67%)",
+      },
+    },
+    fontFamily: {
+      karla: ["Karla", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
